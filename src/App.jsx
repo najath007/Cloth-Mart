9812b2c9794822a9e1a90e4a0f426eac509b51ee
Najath <najathniju007@gmail.com>
2025-11-26 15:22:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='shadow-xl'>
+        <img className='w-24' src="logo.png" alt="" />
+    </div>
+  )
+}
